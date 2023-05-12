fd875a7e51c001472d387a63b4d8c5c3cfaa979f
Curly <84679667+fatihgocerr@users.noreply.github.com>
2023-05-12 15:52:12 +0300

    dashboard first commit

--- dashboard/pages/category/index.js
+++ dashboard/pages/category/index.js
@@ -2,6 +2,7 @@ import Category from "../../components/category/Category";
 import PageLayout from "../../layouts/PageLayout";
 
 export default function CategoryPage() {
+ //log
  return (
   <PageLayout title='Category'>
    <Category />
